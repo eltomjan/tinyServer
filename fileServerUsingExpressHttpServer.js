e60c41e88a76893817ad0d64459b282c431f0c97
Jan Elzein <elzein.eltom@gmail.com>
2024-01-22 11:46:37 +0100

    Update

--- fileServerUsingExpressHttpServer.js
+++ fileServerUsingExpressHttpServer.js
@@ -3,6 +3,27 @@ const app = express();
 const restore = require('./restore');
 const fs = require('fs');
 
+function scheduleFunction(hour, minute, second, func) {
+  const now = new Date();
+
+  const scheduledTime = new Date();
+  scheduledTime.setHours(hour);
+  scheduledTime.setMinutes(minute);
+  scheduledTime.setSeconds(second);
+
+  if (now > scheduledTime) {
+      scheduledTime.setDate(scheduledTime.getDate() + 1);
+  }
+  const delay = scheduledTime.getTime() - now.getTime();
+
+  console.log("Scheduling next function call: " + scheduledTime.toString());
+
+  setTimeout(async () => {
+      await func();
+      scheduleFunction(hour, minute, second, func);
+  }, delay);
+}
+
 async function load()
 {
   const dataDir = "output/data"
@@ -10,6 +31,9 @@ async function load()
     fs.mkdirSync(dataDir);
   }
 
+  scheduleFunction(10, 0, 0, async () => {
+    await restore.fetchData();
+  });
   await restore.fetchData();
 
   app.use(express.static("output"));
