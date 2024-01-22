051f174c649a3a1821b48fbf30b9fae56d555a32
Jan Elzein <elzein.eltom@gmail.com>
2024-01-22 11:35:20 +0100

    Missing data dir

--- fileServerUsingExpressHttpServer.js
+++ fileServerUsingExpressHttpServer.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const restore = require('./restore');
+const fs = require('fs');
 
 async function load()
 {
+  const dataDir = "output/data"
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+  }
+
   await restore.fetchData();
 
   app.use(express.static("output"));
